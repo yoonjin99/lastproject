0d30b9eb0f78991be37e13206f0b47c60b73fda5
yoonjin99 <dbswls1999@naver.com>
2022-04-27 16:37:38 +0900

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <template>
-<!--  <LazyHydrate never >-->
+  <LazyHydrate never >
     <Tutorial/>
-<!--  </LazyHydrate>-->
+  </LazyHydrate>
 </template>
 
 <script>
-import LazyHydrate from 'vue-lazy-hydration';
-
 export default {
   name: 'IndexPage',
-  components:{
-    LazyHydrate
-  }
 }
 </script>
