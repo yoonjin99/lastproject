02c787f54f6e8b142546d69ed893e6ffa136b66f
yoonjin99 <dbswls1999@naver.com>
2022-04-27 16:37:02 +0900

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: false,
+  components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
